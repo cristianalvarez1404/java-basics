f7aaca4633a526c35b9750657f366b02a0852755
CristianAlvarez <cristianandres1496@gmail.com>
2025-06-19 17:16:48 -0500

    java basics

---
+++ src/Main1.java
@@ -0,0 +1,43 @@
+public class Main1 {
+    public static void main(String[] args){
+        // THIS IS MY JAVA PROGRAM
+        /*
+            This is a multi-line comment
+            System.out.print("I like pizza!\n");
+            System.out.print("It's really good!");
+            System.out.println("Buy");
+        */
+
+        /*Variables */
+
+        int age = 35;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = 12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        if(isStudent){
+            System.out.println("You are student");
+        }
+        else {
+            System.out.println("You are not student");
+        }
+
+        String name = "Bro Code";
+        String food = "pizza";
+        String email = "fake@gmail.com";
+
+        System.out.println(name);
+    }
+
+}
